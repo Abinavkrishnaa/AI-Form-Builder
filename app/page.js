200b66032bab31e384da26c1765a53cd00e0d12f
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-05-31 22:01:56 +0530

    add header component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      Hello
+      <Hero />
+      
     </div>
   );
 }
