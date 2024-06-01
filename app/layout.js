2fd202a2f0c6f38e6df09f4a0010927bc777b98b
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-06-01 23:09:40 +0530

    created body section and updated UI

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +12,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={inter.className}>
         <Header />
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
