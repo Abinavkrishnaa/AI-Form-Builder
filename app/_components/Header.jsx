ecbdd9d9ae0543b0d8665d5a56f32bdde15b8bd9
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-06-02 23:00:28 +0530

    clerk-1

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,13 +1,22 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import { UserButton, useUser } from '@clerk/nextjs'
 import Image from 'next/image'
 import React from 'react'
 
 function Header() {
+  const {user,isSignedIn} = useUser();
   return (
     <div className='p-5 border-b shadow-sm'>
         <div className='flex items-center justify-between'>
             <Image src={'/logo.svg'} width={180} height={50} alt="logo" />
-            <Button className="hover:bg-gray-700">Get Started</Button>
+            {isSignedIn? 
+            <div className='flex items-center gap-5'>
+            <Button variant="outline">DashBoard</Button> 
+            <UserButton />
+            </div>:
+            <Button>Get Started</Button>
+            }
         </div>
     </div>
   )
