02853337f25d0db929f7dc3e275f6ca8f38a1f82
Abinavkrishnaa <abinavkrishna2003@gmail.com>
2024-06-06 22:08:39 +0530

    .

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-
     <html lang="en">
       <body className={inter.className}>
         <Header />
